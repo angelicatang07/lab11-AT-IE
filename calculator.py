793568d68f742dab5d397eaed39ca0da8ce2f2fc
angelica tang <angelicatang07@gmail.com>
2025-11-14 22:01:13 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,36 @@
+# https://github.com/angelicatang07/lab11-AT-IE.git
+#Partner 1: Angelica Tang
+#Partner 2: Isabel Espinoza
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+import math
 
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("The base must be positive and not equal to 1")
+    if b <= 0:
+        raise ValueError("The argument must be positive")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
 
